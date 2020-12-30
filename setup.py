5330af8b6908d9be938f397a388adca8a496f056
Denis Efremov <efremov@linux.com>
2020-12-30 09:55:39 +0300

    Change version to 0.1.2
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cvehound',
-    version='0.1.1',
+    version='0.1.2',
     author='Denis Efremov',
     author_email='efremov@linux.com',
     url='http://github.com/evdenis/cvehound',
