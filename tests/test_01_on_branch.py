b743c6c9bafd52e1cffde27b40521f4c45a32381
Denis Efremov <efremov@linux.com>
2021-01-08 15:25:37 +0300

    Add CVE-2019-12455
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_01_on_branch.py
+++ tests/test_01_on_branch.py
@@ -9,6 +9,9 @@ from cvehound import check_cve
         ('origin/linux-4.4.y', 'CVE-2020-27777'),
         ('origin/linux-4.9.y', 'CVE-2020-27777'),
         ('origin/linux-4.4.y', 'CVE-2020-29569'),
+        ('origin/linux-4.19.y', 'CVE-2019-12455'),
+        ('origin/linux-4.14.y', 'CVE-2019-12455'),
+        ('origin/linux-4.9.y', 'CVE-2019-12455'),
     ]
 )
 def test_on_branch(repo, branch, cve):
