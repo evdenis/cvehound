6521f189008b402b5aafad83b510d0b37796844f
Denis Efremov <efremov@linux.com>
2021-01-13 15:56:56 +0300

    tests: check that rule shouldn't detect CVE on fixes~ commit
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_04_on_fixes.py
+++ tests/test_04_on_fixes.py
@@ -10,6 +10,12 @@ def test_on_fixes(repo, cve):
     meta = get_rule_metadata(cve)
     if 'fixes' not in meta:
         pytest.skip('No Fixes/Detect-To tag')
+    fixes = meta['fixes']
 
-    repo.git.checkout(meta['fixes'])
+    repo.git.checkout(fixes)
     assert check_cve(repo.working_tree_dir, cve) == True, 'fails to detect on fixes tag'
+
+    if fixes != 'v2.6.12-rc2' and \
+       fixes != '1da177e4c3f41524e886b7f1b8a0c1fc7321cac2':
+        repo.git.checkout('HEAD~')
+        assert check_cve(repo.working_tree_dir, cve) == False, 'detects on fixes~ tag'
