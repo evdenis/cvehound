2310ba74cccb11d11a3cd8748f4c2122b60a545a
Denis Efremov <efremov@linux.com>
2021-08-24 19:05:19 +0300

    Update update_metadata script
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- cvehound/scripts/update_metadata.py
+++ cvehound/scripts/update_metadata.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import pkg_resources
 import gzip
@@ -8,7 +9,16 @@ import json
 import subprocess
 from datetime import datetime
 
-def main(args=sys.argv[1:]):
+def get_commit_date(repo, commit):
+    return int(subprocess.check_output(
+            ['git', 'show', '-s', '--format=%ct', commit], cwd=repo, stderr=subprocess.DEVNULL, universal_newlines=True
+    ).strip())
+
+def main(args=sys.argv):
+    if len(args) < 2 or not os.path.isdir(os.path.join(args[1], '.git')):
+        print('Usage: {} <kernel_repo_dir>'.format(args[0]), file=sys.stderr)
+        exit(1)
+    repo = args[1]
     filename = pkg_resources.resource_filename('cvehound', 'data/kernel_cves.json.gz')
 
     with urllib.request.urlopen('https://github.com/nluedtke/linux_kernel_cves/raw/master/data/kernel_cves.json') as fh:
@@ -16,11 +26,9 @@ def main(args=sys.argv[1:]):
 
     for cve, info in js.items():
         fix = info.get('fixes', '')
-        if fix:
+        if fix and args[0]:
             try:
-                info['fix_date'] = int(subprocess.check_output(
-                    ['git', 'show', '-s', '--format=%ct', fix], cwd=args[0], stderr=subprocess.DEVNULL, universal_newlines=True
-                ).strip())
+                info['fix_date'] = get_commit_date(repo, fix)
             except Exception:
                 pass
 
@@ -28,4 +36,4 @@ def main(args=sys.argv[1:]):
         json.dump(js, fh)
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv)
