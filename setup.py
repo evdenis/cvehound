82074ed5129a7670c7a5526eb1634dc08a6b83fd
Denis Efremov <efremov@linux.com>
2021-09-10 23:03:39 +0300

    setup.py: drop python 3.5 support
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
