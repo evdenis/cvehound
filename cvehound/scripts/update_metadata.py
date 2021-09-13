d7030a6599d4df279424bd0e3d24e4ccfc0ff148
Denis Efremov <efremov@linux.com>
2021-09-13 23:43:23 +0300

    update_metadata: take info about existing exploits from FSTEC's BDU
    Take info about exploits from https://bdu.fstec.ru/vul
    
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- cvehound/scripts/update_metadata.py
+++ cvehound/scripts/update_metadata.py
@@ -4,10 +4,46 @@ import os
 import sys
 import pkg_resources
 import gzip
-import urllib.request
 import json
 import subprocess
+from urllib.request import urlopen, Request
 from datetime import datetime
+import lxml.etree as etree
+from io import BytesIO
+from zipfile import ZipFile
+
+def get_exploit_status_from_fstec():
+    req = Request('https://bdu.fstec.ru/files/documents/vulxml.zip', headers={'User-Agent': 'Mozilla/5.0'})
+    with urlopen(req) as uh:
+        with ZipFile(BytesIO(uh.read())) as zh:
+            with zh.open('export/export.xml') as fh:
+                tree = etree.parse(fh)
+
+    public = set()
+    private = set()
+    for item in tree.xpath('//vul'):
+        bdu_id = item.xpath('identifier/text()')[0]
+        cve_id = None
+        for vuln_id in item.xpath('identifiers/identifier'):
+            if 'CVE' == vuln_id.get('type'):
+                cve_id = vuln_id.text
+                break
+        is_linux = False
+        for name in item.xpath('vulnerable_software/soft/name/text()'):
+            if name == 'Linux' or name == 'linux':
+                is_linux = True
+        if not is_linux:
+            continue
+        if not cve_id:
+            continue
+
+        exploit_status = item.xpath('exploit_status/text()')[0]
+        if 'открыт' in exploit_status: # 'открытом' == 'public'
+            public.add(cve_id)
+        elif 'уществует' in exploit_status: # == exists
+            private.add(cve_id)
+
+    return public, private
 
 def get_commit_date(repo, commit):
     return int(subprocess.check_output(
@@ -21,7 +57,9 @@ def main(args=sys.argv):
     repo = args[1]
     filename = pkg_resources.resource_filename('cvehound', 'data/kernel_cves.json.gz')
 
-    with urllib.request.urlopen('https://github.com/nluedtke/linux_kernel_cves/raw/master/data/kernel_cves.json') as fh:
+    public, private = get_exploit_status_from_fstec()
+
+    with urlopen('https://github.com/nluedtke/linux_kernel_cves/raw/master/data/kernel_cves.json') as fh:
         js = json.loads(fh.read().decode('utf-8'))
 
     for cve, info in js.items():
@@ -31,6 +69,7 @@ def main(args=sys.argv):
                 info['fix_date'] = get_commit_date(repo, fix)
             except Exception:
                 pass
+        info['exploit'] = cve in public or cve in private
 
     with gzip.open(filename, 'wt', encoding='utf-8') as fh:
         json.dump(js, fh)
