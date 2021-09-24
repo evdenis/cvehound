364de06aac4d09486147b6e1d071e80c3f04b6b2
Denis Efremov <efremov@linux.com>
2021-09-25 00:53:56 +0300

    Fix test_metadata test on .grep rules
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_00_metadata.py
+++ tests/test_00_metadata.py
@@ -11,9 +11,13 @@ def test_metadata(hound, cve):
     assert 'fix' in meta, 'no "Fix:" tag in the rule'
     assert 'fixes' in meta, 'no "Fixes:" or "Detect-To:" tag in the rule'
 
+    rule = hound.get_rule(cve)
+    if rule.endswith('.grep'):
+        return
+
     found = False
     cve_id = re.compile(r'CVE-\d{4}-\d{4,7}')
-    with open(hound.get_rule(cve), 'rt') as fh:
+    with open(rule, 'rt') as fh:
         for line in fh:
             res = cve_id.search(line)
             if res:
