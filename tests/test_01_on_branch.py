75adeb055ee209b25b19f5eaf85d2f864aadfb61
Denis Efremov <efremov@linux.com>
2021-10-02 16:10:17 +0300

    Remove DISPUTED CVE-2019-12455
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_01_on_branch.py
+++ tests/test_01_on_branch.py
@@ -12,9 +12,9 @@ from cvehound.exception import UnsupportedVersion
         ('CVE-2019-15924', 'stable/linux-4.4.y'),
         ('CVE-2020-27777', 'stable/linux-4.4.y'),
         ('CVE-2020-27777', 'stable/linux-4.9.y'),
-        ('CVE-2019-12455', 'stable/linux-4.9.y'),
-        ('CVE-2019-12455', 'stable/linux-4.14.y'),
-        ('CVE-2019-12455', 'stable/linux-4.19.y'),
+        ('CVE-2019-12455', 'stable/linux-4.9.y'), # Disputed
+        ('CVE-2019-12455', 'stable/linux-4.14.y'), # Disputed
+        ('CVE-2019-12455', 'stable/linux-4.19.y'), # Disputed
         ('CVE-2021-22543', 'stable/linux-4.9.y'),
         ('CVE-2019-12819', 'origin/master'), # https://lore.kernel.org/netdev/20210926045313.2267655-1-yanfei.xu@windriver.com/raw
     ]
