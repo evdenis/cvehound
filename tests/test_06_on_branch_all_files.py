8a47abd722307591b4109551de0a2e540015c838
Denis Efremov <efremov@linux.com>
2022-05-14 20:59:26 +0400

    Add CVE-2022-1652
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_06_on_branch_all_files.py
+++ tests/test_06_on_branch_all_files.py
@@ -17,6 +17,12 @@ from cvehound.exception import UnsupportedVersion
         ('CVE-2022-0812', 'stable/linux-4.19.y'),
         ('CVE-2022-0812', 'stable/linux-4.14.y'),
         ('CVE-2022-0812', 'stable/linux-4.9.y'),
+        ('CVE-2022-1652', 'stable/linux-5.15.y'),
+        ('CVE-2022-1652', 'stable/linux-5.10.y'),
+        ('CVE-2022-1652', 'stable/linux-5.4.y'),
+        ('CVE-2022-1652', 'stable/linux-4.19.y'),
+        ('CVE-2022-1652', 'stable/linux-4.14.y'),
+        ('CVE-2022-1652', 'stable/linux-4.9.y'),
     ]
 )
 def test_on_branch(hound, branch, cve):
