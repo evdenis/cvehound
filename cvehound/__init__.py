b3076ec539d21147c15f0f031e5cc107ce7e2aeb
Denis Efremov <efremov@linux.com>
2022-06-04 18:23:47 +0400

    CVEhound 1.1.0
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- cvehound/__init__.py
+++ cvehound/__init__.py
@@ -18,7 +18,7 @@ from cvehound.util import get_spatch_version, get_rule_cves, get_cves_metadata, 
 from cvehound.kbuild import KbuildParser
 from cvehound.config import Config
 
-__VERSION__ = '1.0.9'
+__VERSION__ = '1.1.0'
 
 
 class CVEhound:
