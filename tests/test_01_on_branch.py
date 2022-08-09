64c7ee8a2941cd1ac861d929c3a00efaba510fa5
Denis Efremov <efremov@linux.com>
2022-08-09 16:48:24 +0400

    Update test exceptions
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_01_on_branch.py
+++ tests/test_01_on_branch.py
@@ -12,31 +12,15 @@ from cvehound.exception import UnsupportedVersion
         ('CVE-2019-12455', 'stable/linux-4.9.y'), # Disputed
         ('CVE-2019-12455', 'stable/linux-4.14.y'), # Disputed
         ('CVE-2019-12455', 'stable/linux-4.19.y'), # Disputed
-        ('CVE-2022-1011', 'stable/linux-4.9.y'),
         ('CVE-2022-0998', 'stable/linux-5.15.y'),
-        ('CVE-2022-0812', 'stable/linux-4.19.y'),
-        ('CVE-2022-0812', 'stable/linux-4.14.y'),
-        ('CVE-2022-0812', 'stable/linux-4.9.y'),
-        ('CVE-2022-1652', 'stable/linux-5.15.y'),
-        ('CVE-2022-1652', 'stable/linux-5.10.y'),
-        ('CVE-2022-1652', 'stable/linux-5.4.y'),
-        ('CVE-2022-1652', 'stable/linux-4.19.y'),
-        ('CVE-2022-1652', 'stable/linux-4.14.y'),
-        ('CVE-2022-1652', 'stable/linux-4.9.y'),
         ('CVE-2021-3635', 'stable/linux-4.14.y'),
         ('CVE-2022-28388', 'stable/linux-4.9.y'),
-        ('CVE-2022-1852', 'stable/linux-5.15.y'),
-        ('CVE-2022-1852', 'stable/linux-5.10.y'),
         ('CVE-2022-1789', 'stable/linux-5.4.y'),
         ('CVE-2022-1789', 'stable/linux-4.19.y'),
-        ('CVE-2021-28660', 'stable/linux-5.15.y'),
         ('CVE-2022-34918', 'stable/linux-5.4.y'),
         ('CVE-2022-34918', 'stable/linux-4.19.y'),
         ('CVE-2022-34918', 'stable/linux-4.14.y'),
         ('CVE-2022-34918', 'stable/linux-4.9.y'),
-        ('CVE-2022-21505', 'stable/linux-5.15.y'),
-        ('CVE-2022-21505', 'stable/linux-5.10.y'),
-        ('CVE-2022-21505', 'stable/linux-5.4.y'),
     ]
 )
 def test_on_branch(hound, branch, cve):
