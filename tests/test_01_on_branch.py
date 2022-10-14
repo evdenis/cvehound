91e12f32b11415e84188b42be379976a702266bf
Denis Efremov <efremov@linux.com>
2022-10-14 21:51:36 +0400

    Add CVE-2022-42722
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_01_on_branch.py
+++ tests/test_01_on_branch.py
@@ -57,6 +57,8 @@ from cvehound.exception import UnsupportedVersion
         ('CVE-2022-42721', 'stable/linux-5.15.y'),
         ('CVE-2022-42721', 'stable/linux-5.10.y'),
         ('CVE-2022-42721', 'stable/linux-5.4.y'),
+        ('CVE-2022-42722', 'stable/linux-5.15.y'),
+        ('CVE-2022-42722', 'stable/linux-5.10.y'),
     ]
 )
 def test_on_branch(hound, branch, cve):
