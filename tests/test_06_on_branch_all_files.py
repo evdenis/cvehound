c7aeefca70f409933b17349c17feb1738d381db2
Denis Efremov <efremov@linux.com>
2023-09-12 09:26:16 +0400

    Add CVE-2023-4133
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_06_on_branch_all_files.py
+++ tests/test_06_on_branch_all_files.py
@@ -29,6 +29,12 @@ from cvehound.exception import UnsupportedVersion
         ('CVE-2023-1989', 'stable/linux-5.15.y'),
         ('CVE-2023-1989', 'stable/linux-6.1.y'),
         ('CVE-2023-23005', 'stable/linux-6.1.y'),
+        ('CVE-2023-4133', 'stable/linux-4.14.y'),
+        ('CVE-2023-4133', 'stable/linux-4.19.y'),
+        ('CVE-2023-4133', 'stable/linux-5.4.y'),
+        ('CVE-2023-4133', 'stable/linux-5.10.y'),
+        ('CVE-2023-4133', 'stable/linux-5.15.y'),
+        ('CVE-2023-4133', 'stable/linux-6.1.y'),
     ]
 )
 def test_on_branch(hound, branch, cve):
