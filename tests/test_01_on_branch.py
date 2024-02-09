111e857de3e618a27a182f151eeb468ce37315ac
Denis Efremov <efremov@linux.com>
2024-02-09 15:14:53 +0400

    tests: Update backported CVEs
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/test_01_on_branch.py
+++ tests/test_01_on_branch.py
@@ -9,25 +9,17 @@ from cvehound.exception import UnsupportedVersion
     [
         ('CVE-2019-12455', 'stable/linux-4.19.y'), # Disputed
         ('CVE-2021-4037', 'stable/linux-4.19.y'),
-        ('CVE-2021-4037', 'stable/linux-5.4.y'),
         ('CVE-2022-0998', 'stable/linux-5.15.y'),
         ('CVE-2022-1789', 'stable/linux-4.19.y'),
         ('CVE-2022-1789', 'stable/linux-5.4.y'),
         ('CVE-2022-3061', 'stable/linux-4.19.y'),
         ('CVE-2022-3061', 'stable/linux-5.4.y'),
-        ('CVE-2022-34918', 'stable/linux-4.19.y'),
-        ('CVE-2022-34918', 'stable/linux-5.4.y'),
-        ('CVE-2023-1989', 'stable/linux-4.19.y'),
-        ('CVE-2023-1989', 'stable/linux-5.4.y'),
-        ('CVE-2023-1989', 'stable/linux-5.10.y'),
-        ('CVE-2023-1989', 'stable/linux-5.15.y'),
-        ('CVE-2023-1989', 'stable/linux-6.1.y'),
-        ('CVE-2023-23005', 'stable/linux-6.1.y'),
         ('CVE-2023-4133', 'stable/linux-4.19.y'),
         ('CVE-2023-4133', 'stable/linux-5.4.y'),
         ('CVE-2023-4133', 'stable/linux-5.10.y'),
         ('CVE-2023-4133', 'stable/linux-5.15.y'),
         ('CVE-2023-4133', 'stable/linux-6.1.y'),
+        ('CVE-2023-23005', 'stable/linux-6.1.y'),
     ]
 )
 def test_on_branch(hound, branch, cve):
