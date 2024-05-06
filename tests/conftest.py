1643c17fea389c40d8b135b332ed2197675afb19
Denis Efremov <efremov@linux.com>
2024-05-06 13:54:07 +0400

    Add CVE-2024-26733
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,8 @@ missing_backports = [
     ('CVE-2024-26595', 'stable/linux-5.10.y'),
     ('CVE-2024-26595', 'stable/linux-5.15.y'),
     ('CVE-2024-26595', 'stable/linux-6.1.y'),
+    ('CVE-2024-26733', 'stable/linux-4.19.y'),
+    ('CVE-2024-26733', 'stable/linux-5.4.y'),
 ]
 
 def mount_tmpfs(target, req_mem_gb):
